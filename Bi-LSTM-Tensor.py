a16c907309fc9199b1216acec10f72eb58e6c070
Lai Wei <lw@quanqutech.com>
2019-08-27 12:06:01 +0800

    尝试加入模型save&load功能。。

--- Bi-LSTM-Tensor.py
+++ Bi-LSTM-Tensor.py
@@ -7,7 +7,7 @@ import numpy as np
 tf.reset_default_graph()
 
 sentence = (
-    'The morning had dawned clear and cold with a crispness that hinted at the end of summer They '
+	'The morning had dawned clear and cold with a crispness that hinted at the end of summer They '
 	'set forth at daybreak to see a man beheaded twenty in all and Bran rode among them nervous with '
 	'excitement This was the first time he had been deemed old enough to go with his lord father and his '
 	'brothers to see the king’s justice done It was the ninth year of summer and the seventh of Bran’s life'
@@ -16,28 +16,28 @@ sentence = (
 word_dict = {w: i for i, w in enumerate(list(set(sentence.split())))}
 number_dict = {i: w for i, w in enumerate(list(set(sentence.split())))}
 n_class = len(word_dict)
-n_step = 5 #len(sentence.split())     # 改成5就出错
+n_step = 5 #len(sentence.split())
 n_hidden = 5
 
 def make_batch(sentence):
-    input_batch = []
-    target_batch = []
+	input_batch = []
+	target_batch = []
 
-    words = sentence.split()
-    for i, word in enumerate(words[:-1]):
-        if i < n_step-1:
-            input = [word_dict[n] for n in words[:(i + 1)]]
-            input = input + [0] * (n_step - len(input))
-        else:
-            input = [word_dict[n] for n in words[(i+1-n_step):(i+1)]]
-        target = word_dict[words[i + 1]]
-        input_batch.append(np.eye(n_class)[input])
-        target_batch.append(np.eye(n_class)[target])
-    return input_batch, target_batch
+	words = sentence.split()
+	for i, word in enumerate(words[:-1]):
+		if i < n_step-1:
+			input = [word_dict[n] for n in words[:(i + 1)]]
+			input = input + [0] * (n_step - len(input))
+		else:
+			input = [word_dict[n] for n in words[(i+1-n_step):(i+1)]]
+		target = word_dict[words[i + 1]]
+		input_batch.append(np.eye(n_class)[input])
+		target_batch.append(np.eye(n_class)[target])
+	return input_batch, target_batch
 
 # Bi-LSTM Model
-X = tf.placeholder(tf.float32, [None, n_step, n_class])
-Y = tf.placeholder(tf.float32, [None, n_class])
+X = tf.placeholder(tf.float32, [None, n_step, n_class], name='xx')
+Y = tf.placeholder(tf.float32, [None, n_class], name='yy')
 #print(X.shape)
 
 W = tf.Variable(tf.random_normal([n_hidden * 2, n_class]))
@@ -69,18 +69,20 @@ model = tf.matmul(outputs, W) + b
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
 optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 
-prediction = tf.cast(tf.argmax(model, 1), tf.int32)
+prediction = tf.cast(tf.argmax(model, 1), tf.int32, name='preds')
 
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
+with tf.Session() as sess:
+	init = tf.global_variables_initializer()
+	sess.run(init)
+	saver = tf.train.Saver()
 
-# Training
-for epoch in range(10000):
-    _, loss = sess.run([optimizer, cost], feed_dict={X: input_batch, Y: target_batch})
-    if (epoch + 1)%1000 == 0:
-        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+	# Training
+	for epoch in range(10000):
+		_, loss = sess.run([optimizer, cost], feed_dict={X: input_batch, Y: target_batch})
+		if (epoch + 1)%1000 == 0:
+			saver.save(sess, 'models/ckp', 1000)
+			print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
 
-predict =  sess.run([prediction], feed_dict={X: input_batch})
-print(sentence)
-print([number_dict[n] for n in [pre for pre in predict[0]]])
+	predict =  sess.run([prediction], feed_dict={X: input_batch})
+	print(sentence)
+	print([number_dict[n] for n in [pre for pre in predict[0]]])
---
+++ load_BLT.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+import tensorflow as tf
+import numpy as np
+tf.reset_default_graph()
+sentence = (
+	'The morning had dawned clear and cold with a crispness that hinted at the end of summer They '
+	'set forth at daybreak to see a man beheaded twenty in all and Bran rode among them nervous with '
+	'excitement This was the first time he had been deemed old enough to go with his lord father and his '
+	'brothers to see the king’s justice done It was the ninth year of summer and the seventh of Bran’s life'
+) # 就是1句，1个str
+word_dict = {w: i for i, w in enumerate(list(set(sentence.split())))}
+number_dict = {i: w for i, w in enumerate(list(set(sentence.split())))}
+n_class = len(word_dict)
+n_step = 5 #len(sentence.split())
+n_hidden = 5
+
+def make_batch(sentence):
+	input_batch = []
+	target_batch = []
+
+	words = sentence.split()
+	for i, word in enumerate(words[:-1]):
+		if i < n_step-1:
+			input = [word_dict[n] for n in words[:(i + 1)]]
+			input = input + [0] * (n_step - len(input))
+		else:
+			input = [word_dict[n] for n in words[(i+1-n_step):(i+1)]]
+		target = word_dict[words[i + 1]]
+		input_batch.append(np.eye(n_class)[input])
+		target_batch.append(np.eye(n_class)[target])
+	return input_batch, target_batch
+
+X = tf.placeholder(tf.float32, [None, n_step, n_class])
+input_batch, target_batch = make_batch(sentence)
+print(len(input_batch), input_batch[0].shape)
+print(len(target_batch), target_batch[0].shape)
+
+with tf.Session() as sess:
+	saver = tf.train.import_meta_graph('models/ckp-1000.meta')
+	saver.restore(sess, tf.train.latest_checkpoint('models'))
+	graph = tf.get_default_graph()
+	X = graph.get_tensor_by_name('xx:0')
+	prediction = graph.get_tensor_by_name('preds:0')
+	
+	predict =  sess.run([prediction], feed_dict={X: input_batch})
+	print(sentence)
+	print([number_dict[n] for n in [pre for pre in predict[0]]])
